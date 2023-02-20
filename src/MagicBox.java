25710a5bcb72e568630fbc17e6c84946b292e093
Martyn022 <mg.sharipov@gmail.com>
2023-02-20 14:05:07 +0500

    Решение pick и random

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,2 +1,47 @@
-public class MagicBox {
+import java.util.Random;
+
+public class MagicBox<T> {
+    private T[] items;
+    private int volue;
+
+    public MagicBox(int volue) {
+        this.volue = volue;
+        this.items = (T[]) new Object[volue];
+    }
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() throws RuntimeException {
+        int elements = 0;
+        for (T e : items) {
+            if (e == null) {
+                elements++;
+            }
+        }
+        if (elements != 0) {
+            throw new RuntimeException("Коробка еще не полна, осталось заполнить " + elements + " элементов!");
+        }
+        return items[random()];
+    }
+
+    private int random() {
+        Random random = new Random();
+        int randomInt = random.nextInt(volue);
+        return randomInt;
+    }
+
+    /*public void show() {
+        for (T e : items) {
+            System.out.println(e);
+        }
+    }*/
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MagicBox<String> boxString = new MagicBox<>(5);
+        MagicBox<Integer> boxInteger = new MagicBox<>(4);
+        MagicBox<String> boxError = new MagicBox<>(2);
+
+        boxString.add("Строка 1");
+        boxString.add("Строка 2");
+        boxString.add("Строка 3");
+        boxString.add("Строка 4");
+        boxString.add("Строка 5");
+
+        String boxMagic = boxString.pick();
+        System.out.println(boxMagic);
+
+        boxInteger.add(1);
+        boxInteger.add(2);
+        boxInteger.add(3);
+        boxInteger.add(4);
+
+        int boxNumber = boxInteger.pick();
+        System.out.println(boxNumber);
+        boxError.add("Ошибка! Error!");
+        boxError.pick();
     }
+
 }
